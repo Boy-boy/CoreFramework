a636a01b0587220958eb10d7279c0bbcd9c78a16
gaobo <278708579@qq.com>
2021-05-22 10:15:19 +0800

    add entity soft deleted

---
+++ CoreFramework/src/Core.Ddd.Domain/Entities/ISoftDeleted.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Ddd.Domain.Entities
+{
+    public interface ISoftDeleted
+    {
+        /// <summary>
+        /// 软删除
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ CoreFramework/src/Core.EntityFrameworkCore/DbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Core.Ddd.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+
+namespace Core.EntityFrameworkCore
+{
+    public static class DbContextExtensions
+    {
+        public static void TrySoftDeleted(this IEnumerable<EntityEntry> entityEntries)
+        {
+            foreach (var entry in entityEntries)
+            {
+                if (entry.State != EntityState.Deleted) continue;
+
+                var entityType = entry.Entity.GetType();
+                if (!typeof(ISoftDeleted).IsAssignableFrom(entityType)) continue;
+
+                if (entry.Entity is ISoftDeleted softDeleted)
+                    softDeleted.IsDeleted = true;
+                entry.State = EntityState.Modified;
+            }
+        }
+    }
+}

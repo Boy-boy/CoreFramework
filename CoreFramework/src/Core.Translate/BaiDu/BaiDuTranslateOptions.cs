befab15dcff6e68e5252d29d2f2e487c9ad6ce63
gaobo <278708579@qq.com>
2021-10-23 14:00:09 +0800

    add translate dll

---
+++ CoreFramework/src/Core.Translate/BaiDu/BaiDuTranslateOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Translate.BaiDu
+{
+    public class BaiDuTranslateOptions
+    {
+        public string Url { get; set; }
+
+        public string AppId { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
---
+++ CoreFramework/src/Core.Translate/ITranslateProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Translate
+{
+    public interface ITranslateProvider
+    {
+        /// <summary>
+        /// 翻译转换
+        /// </summary>
+        /// <param name="original">原始数据</param>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>若出现错误，将以original输出</returns>
+        Task<List<string>> TranslateAsync(string original,
+           TranslateQueryDto query,
+           CancellationToken cancellationToken = default);
+    }
+}

866f6b7d34749f896f12b107b3134fdf97d75044
gaobo <278708579@qq.com>
2021-10-25 13:46:23 +0800

    add Recursive

---
+++ CoreFramework/src/Core.Infrastructure/Recursive/RecursiveLoader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Infrastructure.Recursive
+{
+    public class RecursiveLoader
+    {
+        public static List<RecursiveModelDescriptor<T>> LoadModels<T>(IEnumerable<T> models)
+        where T : IRecursiveModel
+        {
+            if (models == null)
+                throw new ArgumentNullException(nameof(models));
+
+            var recursiveModelDescriptors = CreateModelDescriptorsAsync(models);
+
+            return recursiveModelDescriptors;
+        }
+
+        private static List<RecursiveModelDescriptor<T>> CreateModelDescriptorsAsync<T>(IEnumerable<T> models)
+            where T : IRecursiveModel
+        {
+            var newModelDescriptors = new List<RecursiveModelDescriptor<T>>();
+
+            var allModels = models.ToList();
+            if (!allModels.Any())
+                return newModelDescriptors;
+
+            var modelDescriptors = new List<RecursiveModelDescriptor<T>>();
+            foreach (var instance in allModels)
+            {
+                modelDescriptors.Add(new RecursiveModelDescriptor<T>(instance));
+            }
+            modelDescriptors = modelDescriptors
+                .OrderBy(p => p.Instance.Id)
+                .ToList();
+
+            var isTrue = true;
+            while (isTrue)
+            {
+                var startModel = modelDescriptors.First();
+                newModelDescriptors.Add(startModel);
+                startModel?.SetDependencies(modelDescriptors);
+
+                if (modelDescriptors.Count <= 0)
+                    isTrue = false;
+            }
+
+            return newModelDescriptors;
+        }
+    }
+}
---
+++ CoreFramework/src/Core.Infrastructure/Recursive/RecursiveModelDescriptor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Infrastructure.Recursive
+{
+    public class RecursiveModelDescriptor<T>
+    where T : IRecursiveModel
+    {
+        public T Instance { get; set; }
+
+        public IReadOnlyList<RecursiveModelDescriptor<T>> Nodes => _nodes;
+
+        private readonly List<RecursiveModelDescriptor<T>> _nodes;
+
+        public RecursiveModelDescriptor(T instance)
+        {
+            Instance = instance ?? throw new ArgumentNullException(nameof(instance)); ;
+            _nodes = new List<RecursiveModelDescriptor<T>>();
+        }
+
+        public void SetDependencies(List<RecursiveModelDescriptor<T>> models)
+        {
+            if (models == null)
+                return;
+
+            var recursiveModelDescriptors = models.FindAll(m => m.Instance.ParentId != null && m.Instance.ParentId.Equals(Instance.Id));
+            _nodes.AddRange(recursiveModelDescriptors);
+
+            models.Remove(this);
+            foreach (var recursiveModelDescriptor in recursiveModelDescriptors)
+            {
+                models.Remove(recursiveModelDescriptor);
+            }
+
+            foreach (var recursiveModelDescriptor in recursiveModelDescriptors)
+            {
+                recursiveModelDescriptor.SetDependencies(models);
+            }
+        }
+    }
+}

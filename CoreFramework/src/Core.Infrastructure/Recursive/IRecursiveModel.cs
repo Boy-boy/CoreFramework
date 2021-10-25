f58192fa76bc4b79f359a67e7aabc4e7bf51dab0
gaobo <278708579@qq.com>
2021-10-25 13:46:23 +0800

    add Recursive

---
+++ CoreFramework/src/Core.Infrastructure/Recursive/IRecursiveModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Infrastructure.Recursive
+{
+    public interface IRecursiveModel
+    {
+        public object Id { get; set; }
+
+        public object ParentId { get; set; }
+    }
+}

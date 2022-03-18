e8845afe637af6046604d763520dcaede7e2bcf9
gaobo <278708579@qq.com>
2022-03-18 16:37:53 +0800

    add excel

---
+++ CoreFramework/src/Core.Excel/ExcelHelper.cs
@@ -0,0 +1,59 @@
+﻿using OfficeOpenXml;
+
+namespace Core.Excel
+{
+    public static class ExcelHelper
+    {
+        /// <summary>
+        /// 传入数据，返回Excel流文件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources">数据源</param>
+        /// <param name="sheetName">sheet名字</param>
+        /// <returns></returns>
+        public static MemoryStream GetExcelMemoryStreams<T>(this IList<T> sources, string sheetName = "sheet1")
+        {
+            var ms = new MemoryStream();
+            if (sources.Any())
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using var package = new ExcelPackage();
+                var sheet = package.Workbook.Worksheets.Add(sheetName);
+
+                //获取传入的数据类型
+                var propertiesList = typeof(T).GetProperties();
+                for (var row = 1; row <= sources.Count + 1; row++)
+                {
+                    var index = 1;
+                    for (var cl = 1; cl <= propertiesList.Length; cl++)
+                    {
+                        //获取备注名字
+                        var hasDisplayName = propertiesList[cl - 1].TryGetExportExcelDisplayName(out var displayName);
+                        //判断字段是否有自定义属性
+                        if (!hasDisplayName) continue;
+                        if (row == 1) //设置表头
+                            sheet.Cells[row, index].Value = displayName;
+                        else
+                        {
+                            //获取字段名字
+                            var name = propertiesList[cl - 1].Name;
+                            //获取对应的值
+                            var value = sources[row - 2]?.GetType().GetProperty(name)?.GetValue(sources[row - 2])?.ToString();
+                            sheet.Cells[row, index].Value = value;
+                        }
+                        index++;
+                    }
+                }
+                //设置Excel列宽
+                sheet.Cells.AutoFitColumns(1.5);
+                package.SaveAs(ms);
+                ms.Position = 0;
+            }
+            else
+                throw new Exception($"{sheetName}暂无数据！");
+
+            return ms;
+        }
+
+    }
+}
---
+++ CoreFramework/src/Core.Excel/ExportExcelColumnAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace Core.Excel
+{
+    /// <summary>
+    /// Excel导出用
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExportExcelColumnAttribute : Attribute
+    {
+        private readonly string _name;
+
+        /// <summary>
+        /// 构造涵数传入值
+        /// </summary>
+        /// <param name="name"></param>
+        public ExportExcelColumnAttribute(string name)
+        {
+            _name = name;
+        }
+
+        public string GetDisplayName()
+        {
+            return _name;
+        }
+    }
+
+
+    public static class ExportExcelColumnExtensions
+    {
+        /// <summary>
+        /// 获取属性设置的导出备注
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <param name="displayName"></param>
+        /// <returns></returns>
+        public static bool TryGetExportExcelDisplayName(this PropertyInfo prop, out string displayName)
+        {
+            if (!prop.IsDefined(typeof(ExportExcelColumnAttribute), true))
+            {
+                displayName = null;
+                return false;
+            }
+            var attribute = (ExportExcelColumnAttribute)prop.GetCustomAttribute(typeof(ExportExcelColumnAttribute), true);
+            displayName = attribute.GetDisplayName();
+            return true;
+        }
+    }
+}
